9aa6855fc940803102ce2f97c0ce7eac6f5c8e46
dani3l5 <daniel.nicholson04@outlook.com>
2018-10-08 19:07:11 +0100

    Added comments

--- seven_segment.py
+++ seven_segment.py
@@ -1,12 +1,19 @@
+#Imports regular expression module
 import re
 
+#Opens the word list and creates a list as a variable
 file = open('words.txt', 'r')
 file_content = file.read()
 list = file_content.split('\n')
 
+#Specifies the banned words in a regex
 banned = re.compile("[gkmqvwxzio]")
+#Creates a dictionary that stores words and their lengths
 longest_acceptable = {'a': 1}
 
+#Iterates through all words and adds them to the
+#dictionary if there are no banned words in them
+#and it they aren't smaller than the biggest word.
 for item in list:
     if banned.search(item) != None:
         continue
@@ -15,11 +22,16 @@ for item in list:
     else:
         longest_acceptable[item] = len(item)
 
+#Stores the largest length as an int
 biggest = max(longest_acceptable.values())
+#Creates an empty string for storing the final result
 longest_words = ""
 
+#Iterates through dictionary and removes the words that
+#are not the longest that have crept thorugh the cracks.
 for k, v in longest_acceptable.items():
     if v == biggest:
         longest_words += k + '\n'
 
+#Prints the final result
 print(longest_words)
